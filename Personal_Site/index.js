50a537aebd3a1bea02330e9c38ba6cbca549d7a3
Brijit Adak <104793623+UnknownCode01@users.noreply.github.com>
2023-10-27 22:28:36 +0530

    Add files via upload
    step 1: install all the node modules using command "npm i" in the terminal
    step 2: run the js file using the command "node index.js" in the terminal
    step 3: go to the browser and type "localhost:3000" and press enter 
    All done

---
+++ Personal_Site/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+  res.render("index.ejs");
+});
+
+app.get("/about",(req,res)=>{
+  res.render("about.ejs");
+});
+
+app.get("/contact",(req,res)=>{
+  res.render("contact.ejs");
+});
+
+app.post("/submit",(req,res)=>{
+  const name = req.body["name"];
+  const email = req.body["email"];
+  const comment = req.body["text"];
+  res.render("comeback.ejs",{name,email,comment});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
